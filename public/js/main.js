009503b2e5d3606c2f108cf63cb94f7547c01e4b
ShidokhtBj <msbajalan79@gmail.com>
2024-06-13 23:20:11 +0330

    v25

--- public/js/main.js
+++ public/js/main.js
@@ -46,13 +46,72 @@ window.addEventListener("scroll", function () {
   lastScrollTop = currentScrollTop;
 });
 
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
+
+/*const email =document.getElementById('email');
+const form =document.getElementById('form');
+const errorElement =document.getElementById('error');
+form.addEventListener('submit', (e) =>{
+  let messages = []
+  if(email.value === '' || email.value === null){
+    messages.push('Email is required')
+  }
+  if(email.value =! isEmail){
+    messages.push('Email is right')
+  }
+  if(messages.length > 0){
+    e.preventDefault()
+    errorElement.innerText =messages.join(', ')
+  }
+ 
+});
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	checkInputs();
+});
+
+form.addEventListener('submit', (e) => {
+	// trim to remove the whitespaces
+	const emailValue = email.value.trim();
+	let messages = []
+	if(emailValue === '') {
+		messages.push('Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+    messages.push('Not a valid email');
+	} else {
+    messages.push(email);
+	}
+	if(messages.length > 0){
+    e.preventDefault()
+    errorElement.innerText =messages.join(', ')
+  }
+});
 
 
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}*/
 
+const form = document.querySelector(".form"),
+  emailField = form.querySelector(".email-field"),
+  emailInput = emailField.querySelector(".email");
 
-
-
-
-
-
-
+function checkEmail() {
+  const pattern = /^[^ ]+@[^ ]+\.com/;
+  if (!emailInput.value.match(pattern)) {
+    return emailField.classList.add("invalid");
+  }
+  emailField.classList.remove("invalid");
+}
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkEmail();
+  emailInput.addEventListener("keyup" , checkEmail)
+  if(!emailField.classList.contains("invalid")){
+    location.href = form.getAttribute("action");
+  }
+});
