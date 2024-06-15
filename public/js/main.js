78391a78f9ac39ce03f5115ffb80f2d08726aa3f
ShidokhtBj <msbajalan79@gmail.com>
2024-06-15 21:21:47 +0330

    v26

--- public/js/main.js
+++ public/js/main.js
@@ -50,51 +50,6 @@ function scrollToTop() {
   window.scrollTo(0, 0);
 }
 
-/*const email =document.getElementById('email');
-const form =document.getElementById('form');
-const errorElement =document.getElementById('error');
-form.addEventListener('submit', (e) =>{
-  let messages = []
-  if(email.value === '' || email.value === null){
-    messages.push('Email is required')
-  }
-  if(email.value =! isEmail){
-    messages.push('Email is right')
-  }
-  if(messages.length > 0){
-    e.preventDefault()
-    errorElement.innerText =messages.join(', ')
-  }
- 
-});
-
-form.addEventListener('submit', e => {
-	e.preventDefault();
-	checkInputs();
-});
-
-form.addEventListener('submit', (e) => {
-	// trim to remove the whitespaces
-	const emailValue = email.value.trim();
-	let messages = []
-	if(emailValue === '') {
-		messages.push('Email cannot be blank');
-	} else if (!isEmail(emailValue)) {
-    messages.push('Not a valid email');
-	} else {
-    messages.push(email);
-	}
-	if(messages.length > 0){
-    e.preventDefault()
-    errorElement.innerText =messages.join(', ')
-  }
-});
-
-
-	
-function isEmail(email) {
-	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-}*/
 
 const form = document.querySelector(".form"),
   emailField = form.querySelector(".email-field"),
@@ -113,5 +68,38 @@ form.addEventListener("submit", (e) => {
   emailInput.addEventListener("keyup" , checkEmail)
   if(!emailField.classList.contains("invalid")){
     location.href = form.getAttribute("action");
+  }
+});
+
+
+let clickme= document.querySelector("#Support_Open");
+let Support_content= document.querySelector(".Support_content");
+let flag=true;
+clickme.addEventListener("click", function () {
+ if(flag){
+  this.innerHTML = ` <svg style="width: 24px; height: 24px; fill: #FFF;"><path fill-rule="evenodd" d="M18.364 7.05L16.95 5.636 12 10.586l-4.95-4.95L5.636 7.05l4.95 4.95-4.95 4.95 1.414 1.414 4.95-4.95 4.95 4.95 1.414-1.414-4.95-4.95 4.95-4.95z" clip-rule="evenodd"></path></svg>`;
+  flag=false;
+  Support_content.style.display = "flex"
+  clickme.style.transform="rotate(180deg)"
+ }else{
+  this.innerHTML = `   <svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" style="width: 24px; height: 24px; fill: #fff;" width="24"  height="24" ><path fill-rule="evenodd" d="M18 17a4.001 4.001 0 01-3.14 3.907A3 3 0 1012 23h2a6 6 0 006-6v-.256a4.502 4.502 0 000-8.488V7a6 6 0 00-6-6h-4a6 6 0 00-6 6v1.256A4.502 4.502 0 005.5 17H7a1 1 0 001-1V9a1 1 0 00-1-1H6V7a4 4 0 014-4h4a4 4 0 014 4v1h-1a1 1 0 00-1 1v7a1 1 0 001 1h1zm.5-2H18v-5h.5a2.5 2.5 0 010 5zM6 10h-.5a2.5 2.5 0 000 5H6v-5zm7 10a1 1 0 11-2 0 1 1 0 012 0z" clip-rule="evenodd"></path></svg>`;
+  flag=true;
+  Support_content.style.display = "none"
+  clickme.style.transform="rotate(365deg)"
+ }
+});
+let closeMe= document.querySelector(".Support-close");
+closeMe.insertAdjacentHTML("afterbegin","&times;");
+closeMe.addEventListener("click",function(e){
+  console.log(e);
+  Support_content.style.display = "none";
+  clickme.innerHTML = `   <svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" style="width: 24px; height: 24px; fill: #fff;" width="24"  height="24" ><path fill-rule="evenodd" d="M18 17a4.001 4.001 0 01-3.14 3.907A3 3 0 1012 23h2a6 6 0 006-6v-.256a4.502 4.502 0 000-8.488V7a6 6 0 00-6-6h-4a6 6 0 00-6 6v1.256A4.502 4.502 0 005.5 17H7a1 1 0 001-1V9a1 1 0 00-1-1H6V7a4 4 0 014-4h4a4 4 0 014 4v1h-1a1 1 0 00-1 1v7a1 1 0 001 1h1zm.5-2H18v-5h.5a2.5 2.5 0 010 5zM6 10h-.5a2.5 2.5 0 000 5H6v-5zm7 10a1 1 0 11-2 0 1 1 0 012 0z" clip-rule="evenodd"></path></svg>`;
+  
+  if(Support_content.style.display === "none"){
+    clickme.style.transform="rotate(365deg)"
+    flag=true;
+  }else{
+    flag=false;
+   clickme.style.transform="rotate(180deg)"
   }
 });
