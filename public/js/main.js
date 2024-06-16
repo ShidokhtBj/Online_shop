a8ab18e3ba77eb730eed47ee0d7e5af7ced8e27a
ShidokhtBj <msbajalan79@gmail.com>
2024-06-16 20:23:54 +0330

    v23

--- public/js/main.js
+++ public/js/main.js
@@ -16,6 +16,7 @@ window.onscroll = function () {
   prevScrollpos = currentScrollPos;
 };
 
+// Navbar Scroll On Tablet
 const headerTop = document.querySelector(".header__top");
 const headerCenter = document.querySelector(".header__center");
 let lastScrollTop = 0;
@@ -46,11 +47,12 @@ window.addEventListener("scroll", function () {
   lastScrollTop = currentScrollTop;
 });
 
+// footer Btn To Top
 function scrollToTop() {
   window.scrollTo(0, 0);
 }
 
-
+// footer Email Form
 const form = document.querySelector(".form"),
   emailField = form.querySelector(".email-field"),
   emailInput = emailField.querySelector(".email");
@@ -71,7 +73,7 @@ form.addEventListener("submit", (e) => {
   }
 });
 
-
+//Support Open & Close
 let clickme= document.querySelector("#Support_Open");
 let Support_content= document.querySelector(".Support_content");
 let flag=true;
@@ -89,9 +91,7 @@ clickme.addEventListener("click", function () {
  }
 });
 let closeMe= document.querySelector(".Support-close");
-closeMe.insertAdjacentHTML("afterbegin","&times;");
 closeMe.addEventListener("click",function(e){
-  console.log(e);
   Support_content.style.display = "none";
   clickme.innerHTML = `   <svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" style="width: 24px; height: 24px; fill: #fff;" width="24"  height="24" ><path fill-rule="evenodd" d="M18 17a4.001 4.001 0 01-3.14 3.907A3 3 0 1012 23h2a6 6 0 006-6v-.256a4.502 4.502 0 000-8.488V7a6 6 0 00-6-6h-4a6 6 0 00-6 6v1.256A4.502 4.502 0 005.5 17H7a1 1 0 001-1V9a1 1 0 00-1-1H6V7a4 4 0 014-4h4a4 4 0 014 4v1h-1a1 1 0 00-1 1v7a1 1 0 001 1h1zm.5-2H18v-5h.5a2.5 2.5 0 010 5zM6 10h-.5a2.5 2.5 0 000 5H6v-5zm7 10a1 1 0 11-2 0 1 1 0 012 0z" clip-rule="evenodd"></path></svg>`;
   
@@ -102,4 +102,28 @@ closeMe.addEventListener("click",function(e){
     flag=false;
    clickme.style.transform="rotate(180deg)"
   }
+});
+
+
+// Location Open & Close
+let locationOpen = document.querySelector(".header__bottom__location");
+let locationClose = document.querySelector(".location-close");
+let locationContent = document.querySelector(".location");
+let cover = document.querySelector(".cover");
+locationOpen.addEventListener("click", function (){
+  window.fullScreen = true;
+  locationContent.style.display="flex"
+  cover.style.display="flex"
+  $('body').addClass('no-scroll');
+  document.getElementById(".cover").requestFullscreen();
+});
+cover.addEventListener("click", function (){
+  locationContent.style.display="none"
+  cover.style.display="none"
+  $('body').removeClass('no-scroll');
+});
+locationClose.addEventListener("click", function (){
+  locationContent.style.display="none"
+  cover.style.display="none"
+  $('body').removeClass('no-scroll');
 });
